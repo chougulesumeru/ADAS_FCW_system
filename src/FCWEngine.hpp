074ff299c69554dc603087621eacf20f172cd7c9
chougulesumeru <chougulesumeru19@gmail>
2026-01-09 13:16:35 +0530

    .hpp created and added

---
+++ src/FCWEngine.hpp
@@ -0,0 +1,28 @@
+/*
+we will define a class that takes the host vehicles speed and
+the distance of a target vehicle to determine the safety state.
+*/
+
+#ifndef FCW_ENGINE_HPP
+#define FCW_ENGINE_HPP
+
+// create a enum
+enum class SafetyState
+{
+    SAFE,
+    WARNING,
+    EMERGENCY_BRAKING
+};
+
+class FCWEngine
+{
+public:
+    // parameters: TTC thresholds in seconds
+    FCWEngine(double warningTTC, double brakingTTC);
+
+    SafetyState monitor(double hostSpeed, double targetDistance, double targetSpeed);
+
+private:
+    double m_warningTTC;
+    double m_brakingTTC;
+}
